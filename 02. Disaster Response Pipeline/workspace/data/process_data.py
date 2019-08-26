1b3aabe07eb2cd0e5e39fd427ff5ce70e1ce21eb
Mohit <47054927+Moraut@users.noreply.github.com>
2019-08-26 16:54:56 +0530

    Bug: Save data function bug fix
    Change dataframe name for loaded database from 'labelledresponses' to 'df'

--- 02. Disaster Response Pipeline/workspace/data/process_data.py
+++ 02. Disaster Response Pipeline/workspace/data/process_data.py
@@ -63,7 +63,7 @@ def save_data(df, database_filename):
 
     #intialize engine
     engine = create_engine('sqlite://'+database_filename)
-    df.to_sql('labeledresponses', engine, index=False, if_exists = 'replace')
+    df.to_sql('df', engine, index=False, if_exists = 'replace')
 
 
 def main():
