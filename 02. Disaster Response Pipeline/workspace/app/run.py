44e315c80987dd9110c1958c2eada151d7f425ef
Mohit <47054927+Moraut@users.noreply.github.com>
2019-08-27 16:23:35 +0530

    feat: First pass
    Started with code completion.

--- 02. Disaster Response Pipeline/workspace/app/run.py
+++ 02. Disaster Response Pipeline/workspace/app/run.py
@@ -2,6 +2,8 @@ import json
 import plotly
 import pandas as pd
 
+import nltk
+nltk.download(['punkt','wordnet','averaged_perceptron_tagger','stopwords'])
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
@@ -10,6 +12,8 @@ from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
+from sklearn.base import BaseEstimator, TransformerMixin
+
 
 
 app = Flask(__name__)
@@ -25,12 +29,33 @@ def tokenize(text):
 
     return clean_tokens
 
+class first_verb(BaseEstimator, TransformerMixin):
+    
+    def first_verb(self, message):
+        sent_list = nltk.sent_tokenize(message)
+        for x in sent_list:
+            pos_tags = nltk.pos_tag(tokenize(x))
+            if len(pos_tags) >0:
+                first_word, first_tag = pos_tags[0]
+                if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                    return True
+            else:
+                False
+        return False
+    
+    def fit(self,X,y=None):
+        return self
+    
+    def transform(self, X):
+        X_tag = pd.Series(X).apply(self.first_verb)
+        return pd.DataFrame(X_tag)
+
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('df', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -39,9 +64,23 @@ model = joblib.load("../models/your_model_name.pkl")
 def index():
     
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
+
+    # create visual: # of related vs not-realted messages
+    related_dict = {0:'not realted', 1:'related'}
+
+    rel_df = df['related'].map(related_dict)
+    related_counts = rel_df.value_counts()
+    related_names  = list(related_counts.index)
+
+    # create visual: top 10 categories percentage distribution of messages (excluding related)
+
+    categories = df.iloc[:,4:]
+    categories_mean = categories.mean().sort_values(ascending=False) [1:11]
+    categories_names = list(categories_mean.index)
+
+
     
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
@@ -51,6 +90,14 @@ def index():
                 Bar(
                     x=genre_names,
                     y=genre_counts
+                )
+            ],
+
+        
+            'data': [
+                Bar(
+                    x=related_names,
+                    y=related_counts
                 )
             ],
 
